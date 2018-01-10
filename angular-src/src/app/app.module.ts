b27ac02268b04e2cda1f0f83fee10a330758adfa
Cali93 <c.armut93@outlook.com>
2018-01-10 11:27:49 +0100

    navbar updated

---
+++ angular-src/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
+
+import { ValidateService } from './services/validate.service';
+import { AuthService } from './services/auth.service';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthGuard } from './guards/auth.guard';
+import { ModsComponent } from './components/mods/mods.component';
+import { AboutComponent } from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ShopComponent } from './components/shop/shop.component';
+
+const appRoutes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'mods', component: ModsComponent},
+  {path:'shop', component: ShopComponent},
+  {path:'about', component: AboutComponent},
+  {path:'contact', component: ContactComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'login', component: LoginComponent},
+  {path:'dashboard', component: DashboardComponent, canActivate:[AuthGuard]},
+  {path:'profile', component: ProfileComponent, canActivate:[AuthGuard]}
+]
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    DashboardComponent,
+    ProfileComponent,
+    ModsComponent,
+    AboutComponent,
+    ContactComponent,
+    ShopComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    RouterModule.forRoot(appRoutes),
+    FlashMessagesModule.forRoot()
+  ],
+  providers: [ValidateService, AuthService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
